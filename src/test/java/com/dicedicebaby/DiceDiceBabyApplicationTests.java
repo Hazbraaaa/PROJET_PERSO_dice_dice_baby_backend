681ddaa39cc15fc5c7eaa9b554829c6fad8b6a02
chazera <hazera.c@gmail.com>
2025-07-17 14:19:52 +0200

    add: architecture

--- src/test/java/com/dicedicebaby/DiceDiceBabyApplicationTests.java
+++ src/test/java/com/dicedicebaby/DiceDiceBabyApplicationTests.java
@@ -1,10 +1,10 @@
-package com.dicedicebaby.backend;
+package com.dicedicebaby;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class DiceDiceBabyApplicationTests {
 
 	@Test
 	void contextLoads() {
